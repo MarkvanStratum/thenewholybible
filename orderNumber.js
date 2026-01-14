722280ad4cb1663fb95ef3756250c3b46e1585d6
MarkvanStratum <markvanstratum67@gmail.com>
2026-01-14 13:20:09 +0100

    Add files via upload

--- orderNumber.js
+++ orderNumber.js
@@ -4,6 +4,20 @@ const path = require("path");
 const COUNTER_PATH = path.join(process.cwd(), "data", "order-counter.json");
 
 function getNextOrderNumber() {
+  // Ensure /data folder exists
+  const dataDir = path.dirname(COUNTER_PATH);
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+  }
+
+  // If file does not exist, create it starting at 580
+  if (!fs.existsSync(COUNTER_PATH)) {
+    fs.writeFileSync(
+      COUNTER_PATH,
+      JSON.stringify({ lastOrderNumber: 580 }, null, 2)
+    );
+  }
+
   const data = JSON.parse(fs.readFileSync(COUNTER_PATH, "utf8"));
   const nextNumber = data.lastOrderNumber + 1;
 
