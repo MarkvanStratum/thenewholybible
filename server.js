3f83c06d1fc8ad609c74997877a031d3d340da61
MarkvanStratum <markvanstratum67@gmail.com>
2026-01-16 18:57:35 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -649,7 +649,7 @@ page1.drawText(getDeliveryRange(orderDate), {
 
 // ORDER NUMBER — precisely aligned inside sentence
 page1.drawText(`#${orderNumber}`, {
-  x: 110,    // more LEFT
+  x: 200,    // more LEFT
   y: 362,   // more UP
   size: 10,
   color: textColor,
